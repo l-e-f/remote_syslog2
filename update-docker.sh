41cc29b7e1baadc738e475a28c3ca5ab9c568382
Ellie Freymond <eleanor.freymond@fulfil.com>
2025-04-21 13:01:53 -0700

    working

---
+++ update-docker.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+
+docker buildx build --platform linux/amd64 -f Dockerfile -t ellief/remote_syslog2:amd64 --push .
+docker buildx build --platform linux/arm64/v8 -f Dockerfile.arm64 -t ellief/remote_syslog2:arm64v8 --push .
+
+docker manifest create ellief/remote_syslog2:latest ellief/remote_syslog2:amd64 ellief/remote_syslog2:arm64
+docker manifest push ellief/remote_syslog2:latest
